d6d677b0c585a8bc7d6f4d78d2f68c293f1fcb23
Hunter Ford <hunter.ford05@gmail.com>
2018-04-03 18:37:56 -0400

    refactor `ActionButtonView` and add comments

--- Portland Rose/Portland Rose/App/Views/ActionButtonView.m
+++ Portland Rose/Portland Rose/App/Views/ActionButtonView.m
@@ -10,20 +10,27 @@
 #import "Palette.h"
 #import "TypeLibrary.h"
 
-NSString * const TEXT_PLACEHOLDER = @"Hello, Puffins!";
+/// Name of XIB file
 NSString * const NAME_NIB = @"ActionButtonView";
+/// Opacity of the button's shadow
 CGFloat const OPACITY_SHADOW = 0.59;
+/// Offset (horizontal and vertical) of the button's shadow
 CGFloat const OFFSET_SHADOW = 3.0;
+/// Radius of the button's shadow
 CGFloat const RADIUS_SHADOW = 12.0;
+/// Padding, as a multiple of the font size, between the top edge of the button and the text
 CGFloat const PADDING_TOP = 1.0;
+/// Padding, as a multiple of the font size, between the leading (left) edge of the button and the text
 CGFloat const PADDING_LEADING = 2.0;
+/// Default button text
+NSString * const TEXT_PLACEHOLDER = @"Hello, Puffins!";
 
 @interface ActionButtonView()
 
+@property (weak, nonatomic) IBOutlet UILabel *labelViewText;
 @property (strong, nonatomic) IBOutlet UIView *view;
-@property (weak, nonatomic) IBOutlet UILabel *label;
-@property (weak, nonatomic) IBOutlet UIView *background;
-@property (weak, nonatomic) IBOutlet UIView *shadow;
+@property (weak, nonatomic) IBOutlet UIView *viewBackground;
+@property (weak, nonatomic) IBOutlet UIView *viewShadow;
 
 
 @end
@@ -47,13 +54,6 @@ CGFloat const PADDING_LEADING = 2.0;
   return self;
 }
 
-- (void) loadNib{
-  [[NSBundle bundleForClass:self.class] loadNibNamed:NAME_NIB owner:self options:nil];
-  [self addSubview:_view];
-  [_view setFrame: self.bounds];
-  [self nibDidLoad];
-}
-
 - (void) awakeFromNib{
   [super awakeFromNib];
   [self refresh];
@@ -64,22 +64,36 @@ CGFloat const PADDING_LEADING = 2.0;
   [self refresh];
 }
 
+/// Initialize the main view from a XIB file
+- (void) loadNib{
+  [[NSBundle bundleForClass:self.class] loadNibNamed:NAME_NIB owner:self options:nil];
+  [self addSubview:_view];
+  [_view setFrame: self.bounds];
+  [self nibDidLoad];
+}
+
+/// Calculate the button's intrinsic size
+- (CGSize) intrinsicContentSize{
+  return [self calculateSize];
+}
+
+/// Refresh the button's appearance
 - (void) refresh{
   CGFloat cornerRadius;
   
   cornerRadius = _view.frame.size.height / 2;
   
   // Update label
-  [_label setTextColor:_colorLabel];
+  [_labelViewText setTextColor:_colorLabel];
   
   // Update shadow view
-  _shadow.layer.shadowColor = _colorBackgroundSecond.CGColor;
-  _shadow.layer.shadowOffset = CGSizeMake(OFFSET_SHADOW, OFFSET_SHADOW);
-  _shadow.layer.shadowRadius = RADIUS_SHADOW;
-  _shadow.layer.shadowOpacity = OPACITY_SHADOW;
-  _shadow.layer.shadowPath = [UIBezierPath bezierPathWithRoundedRect: _shadow.bounds cornerRadius:cornerRadius].CGPath;
-  _shadow.layer.shouldRasterize = true;
-  _shadow.layer.rasterizationScale = UIScreen.mainScreen.scale;
+  _viewShadow.layer.shadowColor = _colorBackgroundSecond.CGColor;
+  _viewShadow.layer.shadowOffset = CGSizeMake(OFFSET_SHADOW, OFFSET_SHADOW);
+  _viewShadow.layer.shadowRadius = RADIUS_SHADOW;
+  _viewShadow.layer.shadowOpacity = OPACITY_SHADOW;
+  _viewShadow.layer.shadowPath = [UIBezierPath bezierPathWithRoundedRect: _viewShadow.bounds cornerRadius:cornerRadius].CGPath;
+  _viewShadow.layer.shouldRasterize = true;
+  _viewShadow.layer.rasterizationScale = UIScreen.mainScreen.scale;
   
   // Update background view
   CAGradientLayer * grad = [CAGradientLayer layer];
@@ -87,10 +101,11 @@ CGFloat const PADDING_LEADING = 2.0;
   grad.colors = @[(id)_colorBackgroundFirst.CGColor, (id)_colorBackgroundSecond.CGColor];
   grad.startPoint = CGPointMake(0,0);
   grad.endPoint = CGPointMake(1,1);
-  [_background.layer insertSublayer:grad atIndex:0];
-  _background.layer.cornerRadius = cornerRadius;
+  [_viewBackground.layer insertSublayer:grad atIndex:0];
+  _viewBackground.layer.cornerRadius = cornerRadius;
 }
 
+/// Perform initial setup once the XIB file has been loaded
 - (void) nibDidLoad{
   Palette * palette;
   
@@ -102,35 +117,33 @@ CGFloat const PADDING_LEADING = 2.0;
   
   // Configure label text
   [self setText: TEXT_PLACEHOLDER];
-  [_label setFont:[TypeLibrary.sharedTypeLibrary fontBody]];
+  [_labelViewText setFont:[TypeLibrary.sharedTypeLibrary fontBody]];
 }
 
 - (void) setText:(NSString *)text{
   _text = text;
-  [_label setText:_text];
+  [_labelViewText setText:_text];
   [self updateSize];
 }
 
+/// Calculate the intrinsic size of the button based on the size of its text label
 - (CGSize) calculateSize {
   CGSize labelSize;
   CGFloat height;
   CGFloat width;
   CGFloat fontSize;
   
-  labelSize = [_label.text sizeWithAttributes:@{NSFontAttributeName: _label.font}];
-  fontSize = _label.font.pointSize;
+  labelSize = [_labelViewText.text sizeWithAttributes:@{NSFontAttributeName: _labelViewText.font}];
+  fontSize = _labelViewText.font.pointSize;
   height = labelSize.height + fontSize * PADDING_TOP * 2;
   width = labelSize.width + fontSize * PADDING_LEADING * 2;
   
   return CGSizeMake(width, height);
 }
 
+/// Update the button's size
 - (void) updateSize{
   [self invalidateIntrinsicContentSize];
-}
-
-- (CGSize) intrinsicContentSize{
-  return [self calculateSize];
 }
 
 @end
