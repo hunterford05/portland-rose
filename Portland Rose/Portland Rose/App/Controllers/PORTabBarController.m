174030ef67910dcabfd5ec3a138d235b06dae3f8
Hunter Ford <hunter.ford05@gmail.com>
2018-04-04 16:02:58 -0400

    connect `TabBarView` to XIB source file
    * update `Demo.storyboard` and `PORTabBarController` demo files:
      demo the new `TabBarView`

--- Portland Rose/Portland Rose/App/Controllers/PORTabBarController.m
+++ Portland Rose/Portland Rose/App/Controllers/PORTabBarController.m
@@ -7,16 +7,25 @@
 //
 
 #import "PORTabBarController.h"
+#import "TabBarView.h"
 
 @interface PORTabBarController ()
+
+@property (strong, nonatomic) IBOutlet TabBarView *viewTabBar;
 
 @end
 
 @implementation PORTabBarController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+  [super viewDidLoad];
+  CGRect frameTabBar;
+  
+  frameTabBar = _viewTabBar.frame;
+  frameTabBar.size.width = self.view.frame.size.width;
+  _viewTabBar.frame = frameTabBar;
+  
+  [self.view addSubview: _viewTabBar];
 }
 
 - (void)didReceiveMemoryWarning {
--- Portland Rose/Portland Rose/App/Views/TabBarView.m
+++ Portland Rose/Portland Rose/App/Views/TabBarView.m
@@ -8,14 +8,57 @@
 
 #import "TabBarView.h"
 
+static NSString * const NAME_NIB = @"TabBarView";
+
+@interface TabBarView()
+
+@property (strong, nonatomic) IBOutlet UIView *view;
+
+@end
+
 @implementation TabBarView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype) initWithCoder:(NSCoder *)aDecoder{
+  self = [super initWithCoder:aDecoder];
+  if (self) {
+    [self loadNib];
+  }
+  return self;
 }
-*/
+
+- (instancetype) initWithFrame:(CGRect)frame{
+  self = [super initWithFrame: frame];
+  if (self){
+    [self loadNib];
+  }
+  return self;
+}
+
+- (void) awakeFromNib{
+  [super awakeFromNib];
+  [self refresh];
+}
+
+- (void) layoutSubviews{
+  [super layoutSubviews];
+  [self refresh];
+}
+
+/// Initialize the main view from a XIB file
+- (void) loadNib{
+  [[NSBundle bundleForClass:self.class] loadNibNamed: NAME_NIB owner:self options:nil];
+  [self addSubview:_view];
+  [_view setFrame: self.bounds];
+  [self nibDidLoad];
+}
+
+- (void) nibDidLoad{
+  
+}
+
+- (void) refresh {
+  
+}
+
 
 @end
