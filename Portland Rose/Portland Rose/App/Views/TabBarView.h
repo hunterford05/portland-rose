ca8038b070718625aa64b84eb70323ab2aea5172
Hunter Ford <hunter.ford05@gmail.com>
2018-04-04 15:09:44 -0400

    add empty `TabBarView` source files

---
+++ Portland Rose/Portland Rose/App/Views/TabBarView.h
@@ -0,0 +1,13 @@
+//
+//  TabBarView.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 04/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarView : UIControl
+
+@end
---
+++ Portland Rose/Portland Rose/App/Views/TabBarView.m
@@ -0,0 +1,21 @@
+//
+//  TabBarView.m
+//  Portland Rose
+//
+//  Created by Hunter Ford on 04/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import "TabBarView.h"
+
+@implementation TabBarView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
