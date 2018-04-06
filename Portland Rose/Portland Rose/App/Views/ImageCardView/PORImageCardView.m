88795321599ef19bf472c9b661a8a229803059ec
Hunter Ford <hunter.ford05@gmail.com>
2018-04-06 15:28:07 -0400

    connect `PORImageCardView` NIB file

--- Portland Rose/Portland Rose/App/Views/ImageCardView/PORImageCardView.m
+++ Portland Rose/Portland Rose/App/Views/ImageCardView/PORImageCardView.m
@@ -8,14 +8,37 @@
 
 #import "PORImageCardView.h"
 
+/// Name of NIB file
+static NSString * const NAME_NIB = @"PORImageCardView";
+
+@interface PORImageCardView()
+
+@property (strong, nonatomic) IBOutlet UIView *view;
+
+@end
+
 @implementation PORImageCardView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype) initWithCoder:(NSCoder *)aDecoder{
+  self = [super initWithCoder:aDecoder];
+  if (self) {
+    [self loadNib];
+  }
+  return self;
 }
-*/
+
+- (instancetype) initWithFrame:(CGRect)frame {
+  self = [super initWithFrame: frame];
+  if (self){
+    [self loadNib];
+  }
+  return self;
+}
+
+- (void) loadNib {
+  [[NSBundle bundleForClass:self.class] loadNibNamed:NAME_NIB owner:self options:nil];
+  [_view setFrame: self.bounds];
+  [self addSubview: _view];
+}
 
 @end
