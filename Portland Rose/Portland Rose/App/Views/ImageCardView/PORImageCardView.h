39dfc43456b0cb6759f1c34dc8088e9100f872b2
Hunter Ford <hunter.ford05@gmail.com>
2018-04-06 15:20:37 -0400

    add blank source files for `PORImageCardView` class
    * `PORImageCardView` cocoa touch class files
    * `PORImageCardView` NIB file

---
+++ Portland Rose/Portland Rose/App/Views/ImageCardView/PORImageCardView.h
@@ -0,0 +1,13 @@
+//
+//  PORImageCardView.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 06/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PORImageCardView : UIView
+
+@end
---
+++ Portland Rose/Portland Rose/App/Views/ImageCardView/PORImageCardView.m
@@ -0,0 +1,21 @@
+//
+//  PORImageCardView.m
+//  Portland Rose
+//
+//  Created by Hunter Ford on 06/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import "PORImageCardView.h"
+
+@implementation PORImageCardView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
