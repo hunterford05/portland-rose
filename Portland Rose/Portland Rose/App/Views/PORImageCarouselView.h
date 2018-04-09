e6df7f3810ff92c4bb0e67f72d2fe69ce81df404
Hunter Ford <hunter.ford05@gmail.com>
2018-04-09 13:12:40 -0400

    create blank `PORImageCarouselView`; add to Demo
    * create blank `PORImageCarouselView` class source files
    * add a `PORImageCarouselView` instance to the Demo interface builder

---
+++ Portland Rose/Portland Rose/App/Views/PORImageCarouselView.h
@@ -0,0 +1,13 @@
+//
+//  PORImageCarouselView.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 09/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PORImageCarouselView : UIPageViewController
+
+@end
