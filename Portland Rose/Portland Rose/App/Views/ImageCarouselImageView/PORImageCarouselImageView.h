32e8bdfb69c79cebbeed406a785b6b1c78c9ef9d
Hunter Ford <hunter.ford05@gmail.com>
2018-04-09 14:32:12 -0400

    add blank `PORImageCarouselImageView` source files
    * `.m` and `.h` cocoa touch files
    * `.xib` NIB file

---
+++ Portland Rose/Portland Rose/App/Views/ImageCarouselImageView/PORImageCarouselImageView.h
@@ -0,0 +1,13 @@
+//
+//  PORImageCarouselImageView.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 09/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PORImageCarouselImageView : UIPageViewController
+
+@end
---
+++ Portland Rose/Portland Rose/App/Views/ImageCarouselImageView/PORImageCarouselImageView.m
@@ -0,0 +1,37 @@
+//
+//  PORImageCarouselImageView.m
+//  Portland Rose
+//
+//  Created by Hunter Ford on 09/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import "PORImageCarouselImageView.h"
+
+@interface PORImageCarouselImageView ()
+
+@end
+
+@implementation PORImageCarouselImageView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
