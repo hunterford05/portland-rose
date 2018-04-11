120d95414cb7df5994b2f680288e265619719d5f
Hunter Ford <hunter.ford05@gmail.com>
2018-04-11 14:22:28 -0400

    connect NIB file to cocoa touch class

--- Portland Rose/Portland Rose/App/Views/ItinerarySummaryView/PORItinerarySummaryView.m
+++ Portland Rose/Portland Rose/App/Views/ItinerarySummaryView/PORItinerarySummaryView.m
@@ -8,14 +8,40 @@
 
 #import "PORItinerarySummaryView.h"
 
+/// NIB name
+static NSString * const NAME_NIB = @"PORItinerarySummaryView";
+
+@interface PORItinerarySummaryView()
+/// Main view
+@property (strong, nonatomic) IBOutlet UIView *view;
+@end
+
 @implementation PORItinerarySummaryView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark - lifecycle
+
+- (instancetype) initWithFrame:(CGRect)frame{
+  self = [super initWithFrame:frame];
+  [self loadNib];
+  return self;
 }
-*/
+
+- (instancetype) initWithCoder:(NSCoder *)aDecoder{
+  self = [super initWithCoder:aDecoder];
+  [self loadNib];
+  return self;
+}
+
+#pragma mark - helpers
+
+- (void) loadNib {
+  [[NSBundle bundleForClass:self.class] loadNibNamed:NAME_NIB owner:self options:nil];
+  [self nibDidLoad];
+}
+
+- (void) nibDidLoad {
+  _view.frame = self.bounds;
+  [self addSubview: _view];
+}
 
 @end
