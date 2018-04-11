2a46f63bc776c3acfb7a86fd82f1b74182997509
Hunter Ford <hunter.ford05@gmail.com>
2018-04-11 14:12:33 -0400

    add new `PORItinerarySummaryView` source files

---
+++ Portland Rose/Portland Rose/App/Views/ItinerarySummaryView/PORItinerarySummaryView.h
@@ -0,0 +1,13 @@
+//
+//  PORItinerarySummaryView.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 11/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PORItinerarySummaryView : UIView
+
+@end
---
+++ Portland Rose/Portland Rose/App/Views/ItinerarySummaryView/PORItinerarySummaryView.m
@@ -0,0 +1,21 @@
+//
+//  PORItinerarySummaryView.m
+//  Portland Rose
+//
+//  Created by Hunter Ford on 11/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import "PORItinerarySummaryView.h"
+
+@implementation PORItinerarySummaryView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
