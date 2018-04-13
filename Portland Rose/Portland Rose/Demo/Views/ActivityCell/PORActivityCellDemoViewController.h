0579d9da69eae7292c8780003950b259408c46fb
Hunter Ford <hunter.ford05@gmail.com>
2018-04-12 21:03:38 -0400

    add demo scene for Activity Cell View
    * add Table View Controller to Demo storyboard
    * connect to the Demos View Controller scene with a segue
    * update the `NAMES_SEGUE` list in `PORDemosViewController`
    * add empty `PORActivityCellDemoViewController` source files

---
+++ Portland Rose/Portland Rose/Demo/Views/ActivityCell/PORActivityCellDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PORActivityCellDemoViewController.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 12/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PORActivityCellDemoViewController : UITableViewController
+
+@end
---
+++ Portland Rose/Portland Rose/Demo/Views/ActivityCell/PORActivityCellDemoViewController.m
@@ -0,0 +1,33 @@
+//
+//  PORActivityCellDemoViewController.m
+//  Portland Rose
+//
+//  Created by Hunter Ford on 12/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import "PORActivityCellDemoViewController.h"
+
+@interface PORActivityCellDemoViewController ()
+
+@end
+
+@implementation PORActivityCellDemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+
+@end
