1da23ae22842da65821f4d3bd89967c7d2ea4940
Hunter Ford <hunter.ford05@gmail.com>
2018-04-19 13:44:29 -0400

    add new `PORNavigationController` files to proj
    * Move `PORTabBarController` source files to new subdirectory
      inside of `App/Controllers`
    * add boilerplate scaffolding for `PORNavigationController` 
      header and implementation files

---
+++ Portland Rose/Portland Rose/App/Controllers/NavigationController/PORNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  PORNavigationController.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 19/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PORNavigationController : UINavigationController
+
+@end
---
+++ Portland Rose/Portland Rose/App/Controllers/NavigationController/PORNavigationController.m
@@ -0,0 +1,37 @@
+//
+//  PORNavigationController.m
+//  Portland Rose
+//
+//  Created by Hunter Ford on 19/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import "PORNavigationController.h"
+
+@interface PORNavigationController ()
+
+@end
+
+@implementation PORNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
