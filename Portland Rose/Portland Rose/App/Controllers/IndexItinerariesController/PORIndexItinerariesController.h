54b3454a72a8b7c5c9cbf26fa9630985bdc1c4c9
Hunter Ford <hunter.ford05@gmail.com>
2018-04-19 17:10:34 -0400

    implement prototype for index itin. scene
    * renders itinerary summary cells
    * selecting a cell triggers a segue

---
+++ Portland Rose/Portland Rose/App/Controllers/IndexItinerariesController/PORIndexItinerariesController.h
@@ -0,0 +1,20 @@
+//
+//  PORIndexItinerariesController.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 19/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PORItinerary.h"
+#import "PORItinerarySummaryCellView.h"
+#warning Remove mocks
+#import "PORItinerary+Mocks.h"
+
+
+@interface PORIndexItinerariesController : UIViewController <UITableViewDataSource, PORItinerarySummaryCellViewDelegate>
+
+@property (nonatomic) NSArray <PORItinerary *> * itineraries;
+
+@end
