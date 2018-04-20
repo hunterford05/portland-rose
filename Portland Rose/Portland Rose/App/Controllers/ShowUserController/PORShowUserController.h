86e93cbf4ac137508ddc4df7fe267818acd51ec2
Hunter Ford <hunter.ford05@gmail.com>
2018-04-20 13:11:18 -0400

    implement `PORShowUserController` prototype
    * add `PORShowUserController` source files
    * add subviews to interface builder scene
    * connect interface builder subviews to `PORShowUserController` 
      properties
    * implement setup methods for `PORShowUserController` to lay out and
      style subviews

---
+++ Portland Rose/Portland Rose/App/Controllers/ShowUserController/PORShowUserController.h
@@ -0,0 +1,16 @@
+//
+//  PORShowUserController.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 20/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import "PORPalette.h"
+#import "PORTypeLibrary.h"
+#import <UIKit/UIKit.h>
+
+
+@interface PORShowUserController : UIViewController
+
+@end
