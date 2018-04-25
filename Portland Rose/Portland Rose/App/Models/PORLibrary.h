fd003c089740c2142ceea75f17fe13083041fe36
Hunter Ford <hunter.ford05@gmail.com>
2018-04-25 17:24:12 -0400

    add empty `PORLibrary` source files

---
+++ Portland Rose/Portland Rose/App/Models/PORLibrary.h
@@ -0,0 +1,13 @@
+//
+//  PORLibrary.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 25/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PORLibrary : NSObject
+
+@end
---
+++ Portland Rose/Portland Rose/App/Models/PORLibrary.m
@@ -0,0 +1,13 @@
+//
+//  PORLibrary.m
+//  Portland Rose
+//
+//  Created by Hunter Ford on 25/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import "PORLibrary.h"
+
+@implementation PORLibrary
+
+@end
