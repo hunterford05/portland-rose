427960323ec3b0d79755b896265074ca5500d459
Hunter Ford <hunter.ford05@gmail.com>
2018-04-25 17:21:41 -0400

    add library demo view controller
    * add `PORLibraryDemoViewController` header and implementation
      source files
    * update library demo table view controller scene class in
      Demo storyboard -> `PORLibraryDemoViewController`

---
+++ Portland Rose/Portland Rose/Demo/Models/PORLibraryDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PORLibraryDemoViewController.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 25/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PORLibraryDemoViewController : UITableViewController
+
+@end
---
+++ Portland Rose/Portland Rose/Demo/Models/PORLibraryDemoViewController.m
@@ -0,0 +1,35 @@
+//
+//  PORLibraryDemoViewController.m
+//  Portland Rose
+//
+//  Created by Hunter Ford on 25/04/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import "PORLibraryDemoViewController.h"
+
+@interface PORLibraryDemoViewController ()
+
+@end
+
+@implementation PORLibraryDemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+  [self setTitle: @"Library Demo"];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+
+
+@end
