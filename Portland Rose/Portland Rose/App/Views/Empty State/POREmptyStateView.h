9b5add9f29211aa1c0e33b5ebd0d652c56a63824
Hunter Ford <hunter.ford05@gmail.com>
2018-05-07 15:17:45 -0400

    add blank `POREmptyStateView` source files to proj
    * create implementation / header files
    * create `.xib` interface builder file

---
+++ Portland Rose/Portland Rose/App/Views/Empty State/POREmptyStateView.h
@@ -0,0 +1,13 @@
+//
+//  POREmptyStateView.h
+//  Portland Rose
+//
+//  Created by Hunter Ford on 07/05/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface POREmptyStateView : UIView
+
+@end
---
+++ Portland Rose/Portland Rose/App/Views/Empty State/POREmptyStateView.m
@@ -0,0 +1,21 @@
+//
+//  POREmptyStateView.m
+//  Portland Rose
+//
+//  Created by Hunter Ford on 07/05/2018.
+//  Copyright © 2018 Useless Corporation. All rights reserved.
+//
+
+#import "POREmptyStateView.h"
+
+@implementation POREmptyStateView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
