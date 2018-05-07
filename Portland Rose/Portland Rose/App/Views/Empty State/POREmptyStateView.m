f968c136702e788b2aa8692615ba74e912df1a8a
Hunter Ford <hunter.ford05@gmail.com>
2018-05-07 15:25:47 -0400

    connect `.xib` file to `POREmptyStateView.m`
    * override `initWithFrame:` and `initWithCoder:`
    * implement `loadNib` and add blank `nibDidLoad`
    * add IB_DESIGNABLE to `POREmptyStateView.h`
    * add a `POREmptyStateView` instance to the appropriate view controller
      in the Demo storyboard

--- Portland Rose/Portland Rose/App/Views/Empty State/POREmptyStateView.m
+++ Portland Rose/Portland Rose/App/Views/Empty State/POREmptyStateView.m
@@ -8,14 +8,47 @@
 
 #import "POREmptyStateView.h"
 
+/// Nib filename
+static NSString * const NAME_NIB = @"POREmptyStateView";
+
+@interface POREmptyStateView()
+
+/// Main view
+@property (strong, nonatomic) IBOutlet UIView *view;
+
+@end
+
 @implementation POREmptyStateView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark - lifecycle
+
+- (instancetype) initWithCoder:(NSCoder *)aDecoder{
+  self = [super initWithCoder:aDecoder];
+  if (self){
+    [self loadNib];
+  }
+  return self;
 }
-*/
+
+- (instancetype) initWithFrame:(CGRect)frame{
+  self = [super initWithFrame:frame];
+  if (self){
+    [self loadNib];
+  }
+  return self;
+}
+
+#pragma mark - helpers
+
+- (void)loadNib{
+  [[NSBundle bundleForClass:self.class] loadNibNamed:NAME_NIB owner:self options:nil];
+  [self addSubview:_view];
+  [_view setFrame: self.bounds];
+  [self nibDidLoad];
+}
+
+- (void)nibDidLoad{
+  
+}
 
 @end
